2144a9d6830e7bfc6dc7b9fbf8633dda9a87772c
Aidil-04 <ahmed_marzuki_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 13:30:04 +0700

    Add files via upload

---
+++ HitungFaktorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HitungFaktorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan bulat positif: ");
+        int n = input.nextInt();
+        
+        long faktorial = 1;
+        
+        if (n < 0) {
+            System.out.println("Faktorial tidak dapat dihitung untuk bilangan negatif.");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                faktorial *= i;
+            }
+            System.out.println("Faktorial dari " + n + " adalah: " + faktorial);
+        }
+        
+        input.close();
+    }
+}
